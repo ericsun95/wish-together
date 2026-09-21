390120277f585d9f35f34d3b4f2fd6b66419e538
ericsun95 <31479694+ericsun95@users.noreply.github.com>
2026-09-21 10:06:10 -0700

    feat: add shared space themes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import "./space.css";
 import "./wish-details.css";
+import "./theme.css";
 
 export const metadata: Metadata = {
   title: "Wish Together",
